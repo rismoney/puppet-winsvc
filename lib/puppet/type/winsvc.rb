89c1d93882bb64ecff91a3947be185c344aaba21
Rich Siegel <rsiegel@ise.com>
2015-02-26 15:37:27 -0500

    type: modify the type to handle additional parameters
    flexibility for installing the service is needed to be able
    to choose using command line sc.exe tool or the dotnet installutil

--- lib/puppet/type/winsvc.rb
+++ lib/puppet/type/winsvc.rb
@@ -15,6 +15,25 @@ Puppet::Type.newtype(:winsvc) do
     end
   end
 
+  newproperty(:path) do
+    desc "The path to the windows service"
+  end
+
+  newparam(:dotnetversion) do
+    desc "the dotnetversion of installutil to use"
+  end
+
+  newparam(:sixtyfourbit) do
+    desc "use 64 bit dotnet"
+  end
+
+  newparam(:method) do
+    desc "use sc or installutil method"
+    validate do |value|
+      raise Puppet::Error, "method must not be empty" if value.empty?
+    end
+  end
+
   newparam(:name, :namevar=>true) do
     desc "The Windows service name (case-sensitive)."
   end
